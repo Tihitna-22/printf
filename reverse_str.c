4dab829dc92fc29927ac0e2308b3310da4eb4c39
dk-Gk <idkin19@gmail.com>
2021-10-05 23:46:19 +0000

    reverse a string

---
+++ reverse_str.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_str - reverses a string
+ *@s: char pointer
+ *@j: counter through string
+ * Return: *s
+ */
+
+void _rev_string(char *s, int *j)
+{
+int l, count;
+l = _string_length(s);
+count = l - 1;
+while (count >= 0)
+{
+_putchar(s[count], j);
+count--;
+}
+}

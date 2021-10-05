6f39260c3cbec1e3acef706c1b26458e11b6330e
Tihitna <71766297+Tihitna-22@users.noreply.github.com>
2021-10-05 17:48:07 +0300

    Update main.h

--- main.h
+++ main.h
@@ -5,10 +5,10 @@
 /*Typedefs*/
 /*Prototypes*/
 int _putchar(char c, int *k);
-void _puts(char *str, int *k);
+void print_string(char *str, int *k);
 int _printf(const char *format, ...);
-void _print_number(int n, int *k);
-void _rev_string(char *s, int *k);
+void print_num(int n, int *k);
+void reverse_str(char *s, int *j);
 int _strlen(char *s);
 void _print_binary(int n, int *k);
 void _rot13(char *s, int *k);

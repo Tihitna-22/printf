85d2d2f85cec8fac2d6fa7866b8fe41321ac26d3
Tihitna-22 <tihitna222sisay@gmail.com>
2021-10-05 05:56:14 -0700

    add  _string_length.c

---
+++ _string_length.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ *
+ * @s: The string to get the length of
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	/* s = "Hello"; */
+	int arrayIndex;
+
+	arrayIndex = 0;
+	/* while the character we're looking at isn't null */
+	while (s[arrayIndex] != '\0')
+	{
+		/* add one more to the count and continue looking */
+		arrayIndex++;
+	}
+
+	return (arrayIndex);
+}

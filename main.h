1632730a09fb5673e231e40cfe27db9f99b51e4c
Tihitna <71766297+Tihitna-22@users.noreply.github.com>
2021-10-05 18:29:32 +0300

    Update main.h

--- main.h
+++ main.h
@@ -4,9 +4,9 @@
 int _putchar(char c, int *k);
 void print_string(char *str, int *k);
 int _printf(const char *format, ...);
-void print_num(int n, int *k);
-void reverse_str(char *s, int *k);
-int _string_length(char *s);
-void _printBinary(int n, int *k);
+void _print_number(int n, int *k);
+void _rev_string(char *s, int *k);
+int _strlen(char *s);
+void _print_binary(int n, int *k);
 void _rot13(char *s, int *k);
 #endif

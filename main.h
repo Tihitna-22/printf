478141f73589432b74071ceb3494572b2f85c59f
dk-Gk <idkin19@gmail.com>
2021-10-06 01:07:48 +0000

    header file

--- main.h
+++ main.h
@@ -1,15 +1,12 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 #include <stdarg.h>
-/*Structs*/
-/*Typedefs*/
-/*Prototypes*/
 int _putchar(char c, int *k);
-void _puts(char *str, int *k);
+void print_string(char *str, int *k);
 int _printf(const char *format, ...);
-void _print_number(int n, int *k);
-void _rev_string(char *s, int *k);
-int _strlen(char *s);
-void _print_binary(int n, int *k);
+void print_num(int n, int *k);
+void reverse_str(char *s, int *k);
+int _string_length(char *s);
+void _printBinary(int n, int *k);
 void _rot13(char *s, int *k);
 #endif

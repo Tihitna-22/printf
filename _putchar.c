2052cd44906415657e7f1870a40686fc30950157
Tihitna <71766297+Tihitna-22@users.noreply.github.com>
2021-10-05 18:45:35 -0700

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -7,11 +7,10 @@
  */
 int _puts(char *str)
 {
-	char *a = str;
-
-	while (*str)
-		_putchar(*str++);
-	return (str - a);
+char *a = str;
+while (*str)
+_putchar(*str++);
+return (str - a);
 }
 
 /**
@@ -23,15 +22,14 @@ int _puts(char *str)
  */
 int _putchar(int c)
 {
-	static int i;
-	static char buf[OUTPUT_BUF_SIZE];
-
-	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
-	{
-		write(1, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+static int i;
+static char buf[OUTPUT_BUF_SIZE];
+if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+{
+write(1, buf, i);
+i = 0;
+}
+if (c != BUF_FLUSH)
+buf[i++] = c;
+return (1);
 }

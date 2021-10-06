92a7588f16ec37582880ef3da711f5302bd19e25
dk-Gk <idkin19@gmail.com>
2021-10-06 04:14:36 +0000

    print string

--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,36 @@
 #include"main.h"
-#include <unistd.h>
+/**
+ * _puts - prints a string with newline
+ * @str: the string to print
+ *
+ * Return: void
+ */
+int _puts(char *str)
+{
+	char *a = str;
+
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
+}
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to be printed
  * @k: counter through string
  * Return: One or -1 if it is error
  */
-int _putchar(char c, int *k)
+int _putchar(char c)
 {
-*k += 1;
-return (write(1, &c, 1));
+static int i;
+static char buf[OUTPUT_BUF_SIZE];
+
+if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+{
+write(1, buf, i);
+i = 0;
+}
+if (c != BUF_FLUSH)
+buf[i++] = c;
+return (1);
 }

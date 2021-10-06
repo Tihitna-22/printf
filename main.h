339c8d4cd0c969a59ffc2d3e1f7cbea63ec4fcba
dk-Gk <idkin19@gmail.com>
2021-10-06 04:11:42 +0000

    header file

---
+++ main.h
@@ -0,0 +1,83 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#define OUTPUT_BUF_SIZE 1024
+/**
+ * struct parameters - parameters struct
+ *
+ * @unsign: flag if unsigned value
+ *
+ * @plus_flag: on if plus_flag specified
+ * @space_flag: on if hashtag_flag specified
+ * @hashtag_flag: on if _flag specified
+ * @zero_flag: on if _flag specified
+ * @minus_flag: on if _flag specified
+ *
+ * @width: field width specified
+ * @precision: field precision specified
+ *
+ * @h_modifier: on if h_modifier is specified
+ * @l_modifier: on if l_modifier is specified
+ *
+ */
+typedef struct parameters
+{
+unsigned int unsign: 1;
+unsigned int plus_flag: 1;
+unsigned int space_flag: 1;
+unsigned int hashtag_flag: 1;
+unsigned int zero_flag: 1;
+unsigned int minus_flag: 1;
+unsigned int width;
+unsigned int precision;
+unsigned int h_modifier: 1;
+unsigned int l_modifier: 1;
+} p_t;
+
+/**
+ * struct specifier - Struct token
+ *
+ * @specifier: format token
+ * @f: The function associated
+ */
+typedef struct specifier
+{
+char *specifier;
+int (*f)(va_list, p_t *);
+} s_t;
+int print_char(va_list list, p_t *par);
+int print_int(va_list list, p_t *par);
+int _puts(char *str);
+int print_string(va_list list, p_t *par);
+int _putchar(int c);
+int print_percent(va_list list, p_t *par);
+int print_hex(va_list list, p_t *par);
+int print_S(va_list ap, params_t *params);
+int print_HEX(va_list list, p_t *par);
+char *convert(long int num, int base, int flags, params_t *par);
+int print_unsigned(va_list list, p_t *par);
+int print_address(va_list list, p_t *par);
+int (*get_specifier(char *s))(va_list list, p_t *par);
+int get_print_func(char *s, va_list list, p_t *par);
+int print_binary(va_list list, p_t *par);
+int print_octal(va_list list, p_t *par);
+int get_flag(char *s, params_t *params);
+int print_from_to(char *start, char *stop, char *except);
+int print_rev(va_list list, p_t *par);
+int print_rot13(va_list list, p_t *par);
+int _isdigit(int c);
+int _strlen(char *s);
+int get_modifier(char *s, p_t *par);
+int print_number(char *str, p_t *par);
+int print_number_right_shift(char *str, p_t *par);
+int print_number_left_shift(char *str, p_t *par);
+char *get_width(char *s, p_t *par, va_list list);
+void init_params(p_t *par, va_list list);
+char *get_precision(char *p, p_t *par, va_list list);
+int _printf(const char *format, ...);
+#endif

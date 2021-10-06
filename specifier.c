51983120d585e1a768693775575488c8045e9662
Tihitna-22 <tihitna222sisay@gmail.com>
2021-10-05 18:30:43 -0700

    modify specifier.c

--- specifier.c
+++ specifier.c
@@ -6,51 +6,53 @@
  *
  * Return: the number of bytes printed
  */
-int (*get_specifier(char *s))(va_list list, p_t *par)
+int (*get_specifier(char *s))(va_list ap, params_t *params)
 {
-specifier_t specifiers[] = {
-{"c", print_char},
-{"d", print_int},
-{"i", print_int},
-{"s", print_string},
-{"%", print_percent},
-{"b", print_binary},
-{"o", print_octal},
-{"u", print_unsigned},
-{"x", print_hex},
-{"X", print_HEX},
-{"p", print_address},
-{"S", print_S},
-{"r", print_rev},
-{"R", print_rot13},
-{NULL, NULL}
-};
-int i = 0;
-while (specifiers[i].specifier)
-{
-if (*s == specifiers[i].specifier[0])
-{
-return (specifiers[i].f);
-}
-i++;
-}
-return (NULL);
+	specifier_t specifiers[] = {
+		{"c", print_char},
+		{"d", print_int},
+		{"i", print_int},
+		{"s", print_string},
+		{"%", print_percent},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"u", print_unsigned},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"p", print_address},
+		{"S", print_S},
+		{"r", print_rev},
+		{"R", print_rot13},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (specifiers[i].specifier)
+	{
+		if (*s == specifiers[i].specifier[0])
+		{
+			return (specifiers[i].f);
+		}
+		i++;
+	}
+	return (NULL);
 }
 
 /**
  * get_print_func - finds the format func
  * @s: the format string
- * @list: argument pointer
- * @par: the parameters struct
+ * @ap: argument pointer
+ * @params: the parameters struct
  *
  * Return: the number of bytes printed
  */
-int get_print_func(char *s, va_list list, p_t *par)
+int get_print_func(char *s, va_list ap, params_t *params)
 {
-int (*f)(va_list, p_t *) = get_specifier(s);
-if (f)
-return (f(ap, par));
-return (0);
+	int (*f)(va_list, params_t *) = get_specifier(s);
+
+	if (f)
+		return (f(ap, params));
+	return (0);
 }
 
 /**
@@ -60,73 +62,77 @@ return (0);
  *
  * Return: if flag was valid
  */
-int get_flag(char *s, p_t *par)
+int get_flag(char *s, params_t *params)
 {
-int i = 0;
-switch (*s)
-{
-case '+':
-i = par->plus_flag = 1;
-break;
-case ' ':
-i = par->space_flag = 1;
-break;
-case '#':
-i = par->hashtag_flag = 1;
-break;
-case '-':
-i = par->minus_flag = 1;
-break;
-case '0':
-i = par->zero_flag = 1;
-break;
-}
-return (i);
+	int i = 0;
+
+	switch (*s)
+	{
+		case '+':
+			i = params->plus_flag = 1;
+			break;
+		case ' ':
+			i = params->space_flag = 1;
+			break;
+		case '#':
+			i = params->hashtag_flag = 1;
+			break;
+		case '-':
+			i = params->minus_flag = 1;
+			break;
+		case '0':
+			i = params->zero_flag = 1;
+			break;
+	}
+	return (i);
 }
 
 /**
  * get_modifier - finds the modifier func
  * @s: the format string
- * @par: the parameters struct
+ * @params: the parameters struct
  *
  * Return: if modifier was valid
  */
-int get_modifier(char *s, p_t *par)
+int get_modifier(char *s, params_t *params)
 {
-int i = 0;
-switch (*s)
-{
-case 'h':
-i = par->h_modifier = 1;
-break;
-case 'l':
-i = par->l_modifier = 1;
-break;
-}
-return (i);
+	int i = 0;
+
+	switch (*s)
+	{
+	case 'h':
+		i = params->h_modifier = 1;
+		break;
+	case 'l':
+		i = params->l_modifier = 1;
+		break;
+	}
+	return (i);
 }
 
 /**
  * get_width - gets the width from the format string
  * @s: the format string
- * @par: the parameters struct
- * @list: the argument pointer
+ * @params: the parameters struct
+ * @ap: the argument pointer
  *
  * Return: new pointer
  */
-char *get_width(char *s, p_t *par, va_list list)
+char *get_width(char *s, params_t *params, va_list ap)
+/* should this function use char **s and modify the pointer? */
 {
-int d = 0;
-if (*s == '*')
-{
-d = va_arg(list, int);
-s++;
-}
-else
-{
-while (_isdigit(*s))
-d = d * 10 + (*s++ - '0');
-}
-par->width = d;
-return (s);
+	int d = 0;
+
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->width = d;
+	return (s);
 }
